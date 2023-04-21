c5d08c15bc23a0b2fd18cf241ae1fd00a2e9beaa
benaygülmez <benay_gulmez@hotmail.com>
2023-04-21 16:28:45 +0300

    odev 4 yanitlar

---
+++ 4. Ödev/4.ÖDEV.sql
@@ -0,0 +1,22 @@
+SELECT DISTINCT replacement_cost FROM film replacement_cost;
+
+SELECT COUNT (DISTINCT replacement_cost) FROM film;
+
+
+SELECT*FROM Film
+WHERE title like 'T%' AND rating = 'G';
+
+SELECT*FROM country
+WHERE LENGTH (country) = 5;
+
+SELECT count(distinct city) FROM city
+WHERE city ILIKE '%r';
+
+
+
+
+
+
+
+
+ 
