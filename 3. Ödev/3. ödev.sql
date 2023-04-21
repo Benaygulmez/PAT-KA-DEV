835977b00c1c057a7026f00a8a3498bb24ba4f7a
benaygülmez <benay_gulmez@hotmail.com>
2023-04-21 15:30:46 +0300

    odev 3 yanitlar

---
+++ 3. Ödev/3. ödev.sql
@@ -0,0 +1,20 @@
+SELECT*FROM country
+WHERE country LIKE 'A%a';
+
+SELECT*FROM country
+WHERE country like '6%n';
+
+SELECT*FROM film
+WHERE title ILIKE '%T%T%T%T%';
+
+SELECT*FROM film
+WHERE title Like 'C%' AND length > 90 AND rental_rate = 2.99
+
+
+
+
+
+
+
+
+ 
